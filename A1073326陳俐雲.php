ea2fdca2af96e1b4a028e0ffb0f67d28cc84d641
veronicatania <61532269+veronicatania@users.noreply.github.com>
2020-03-11 19:19:57 +0800

    Add files via upload
    陳俐雲A1073326

---
+++ A1073326陳俐雲.php
@@ -0,0 +1,59 @@
+<head>
+
+<title>NUK Registration Form</title>
+
+<style>
+p.a {
+	font: 70px arial, sans-serif;
+}
+p.b{
+	font : 30px, sans-serif;
+}
+</style>
+
+<body style="background-color: pink">
+	</body>
+
+</head>
+
+<form action="submit,php" method="POST">
+
+l<center><font size="36"> NUK Registration Form</center></font><br/>
+
+	please input your name: <input type="text" placeholder="English only" name="name"><br/>
+
+<br/> please select birthday date <input type="date"><br/>
+<br/> please input your address <input type="text"><br/>
+<br/> please input your phone number <input type="number"><br/>
+<br/> please select your gender <br/>
+Male<input type=radio name="gender" value="m">
+female<input type=radio name="gender" value="f"><br/>
+<br/> please input your email address <input type="email"><br/>
+<br/> please select your image <input type="image"><br/>
+<br/> please select your education background <br/>
+High school<input type=radio name="education background" value="h">
+Bachelor<input type=radio name="education background" value="b">
+Master<input type=radio name="education background" value="m"><br/>
+
+<br/> Please select your interest : </br>
+watching <input type="checkbox" name="int1" value="watch">
+swimming <input type="checkbox" name="int2" value="swim">
+travelling <input type="checkbox" name="int3" value="travel">
+reading <input type="checkbox" name="int4" value="read">
+other <input type="checkbox" name="int5" value="other">
+<br/>
+
+<br/>
+<textarea rows="20" cols="100" name="comment">
+	please give us your feedback and comments
+</textarea>
+
+<br/>
+<br/>
+</br>
+<input type="reset" value="clear"><input ="submit" value="send data">
+<br/>
+
+</form>
+
+
